a138b8039f6b9b8ba67d2a9c362cd6db5eb2a5b8
Your Name <you@example.com>
2022-10-24 17:46:18 +0100

    Pataisyta kabutes " buvo viena per daug. New func

--- kinotekos_backendas.py
+++ kinotekos_backendas.py
@@ -1,17 +1,23 @@
 import sqlite3
 
+
 con = sqlite3.connect("kinoteka.db")
 kursorius = con.cursor()
 
-kursorius.execute(""""
+kursorius.execute("""
     CREATE TABLE IF NOT EXISTS
     kinoteka (id INTEGER PRIMARY KEY,
     kino_ID text,
-    kino_Pavadinimas text,
+    kino_pavadinimas text,
     isleidimo_data text,
     kino_biudzetas text,
-    Reitingas text)
+    reitingas text)
     """
     )
 con.commit()
 con.close()
+
+def prideti_filma(kino_ID, kino_pavadinimas, isleidimo_data, kino_biudzetas, reitingas):
+    kursorius.execute("INSERT INTO kinoteka VALUE (NUL, ?,?,?,?,?)", (kino_ID, kino_pavadinimas, isleidimo_data, kino_biudzetas, reitingas))
+    con.commit()
+    con.close()
